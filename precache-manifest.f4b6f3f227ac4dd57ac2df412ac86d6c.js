ad27ba54564e51c594b3b3fa53dc1c05ba79277c
daoud1581 <david11111988@hotmail.com>
2020-04-17 11:44:55 +0000

    Updates

---
+++ precache-manifest.f4b6f3f227ac4dd57ac2df412ac86d6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e14db2a6d2ca7bc7c015b0dddc081a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "98901644d79558530e4b",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98901644d79558530e4b",
+    "url": "/static/js/main.a8c453b4.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
